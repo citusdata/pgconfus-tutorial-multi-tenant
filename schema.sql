58167dabde3b4939ac55958812ce64271ecc187a
Lukas Fittl <lukas@fittl.com>
2016-11-14 10:13:41 -0800

    Updated schema.sql.

--- schema.sql
+++ schema.sql
@@ -1,9 +1,3 @@
-CREATE TYPE category AS ENUM ('Art', 'Electronics', 'Entertainment', 'Fashion', 'Home & Garden', 'Sporting Goods', 'Other');
-CREATE TYPE status AS ENUM ('open', 'processing', 'fulfilled');
-
-SELECT citus_run_on_all_workers($$CREATE TYPE category AS ENUM ('Art', 'Electronics', 'Entertainment', 'Fashion', 'Home & Garden', 'Sporting Goods', 'Other')$$);
-SELECT citus_run_on_all_workers($$CREATE TYPE status AS ENUM ('open', 'processing', 'fulfilled')$$);
-
 CREATE TABLE users (
   user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
   email text NOT NULL UNIQUE,
@@ -17,7 +11,7 @@ CREATE TABLE stores (
   store_id uuid NOT NULL DEFAULT uuid_generate_v4(),
   user_id uuid NOT NULL DEFAULT uuid_generate_v4(),
   name text NOT NULL,
-  category category NOT NULL,
+  category text NOT NULL,
   created_at timestamptz NOT NULL DEFAULT now(),
   updated_at timestamptz NOT NULL DEFAULT now(),
   PRIMARY KEY (store_id)
@@ -43,7 +37,7 @@ SELECT create_distributed_table('products', 'store_id');
 CREATE TABLE orders (
   store_id uuid NOT NULL DEFAULT uuid_generate_v4(),
   order_id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  status status NOT NULL,
+  status text NOT NULL,
   total_amount numeric(20,2) NOT NULL,
   shipping_address text NOT NULL,
   billing_address text NOT NULL,
